4ff5322cbba19ecf9eac8362f311e7f0f0a12825
timataliatov <ttaliatov@gmail.com>
2024-10-09 17:25:42 +0200

    db changes v2

--- src/app.js
+++ src/app.js
@@ -4,12 +4,15 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const errorHandler = require('./middleware/errorHandler');
+const http = require('http');
+const { neon } = require('@neondatabase/serverless');
 
 const userRoutes = require('./routes/users');
 const commentRoutes = require('./routes/comments');
 const postRoutes = require('./routes/posts');
 
 const app = express();
+const sql = neon(process.env.DATABASE_URL);
 
 app.use(cors());
 app.use(helmet());
@@ -21,8 +24,21 @@ app.use('/api/users', userRoutes);
 app.use('/api/comments', commentRoutes);
 app.use('/api/posts', postRoutes);
 
+// add a new route to check database version
+app.get('/db-version', async (req, res) => {
+  try {
+    const result = await sql`SELECT version()`;
+    const { version } = result[0];
+    res.status(200).send(version);
+  } catch (error) {
+    console.error('Error fetching database version:', error);
+    res.status(500).send('Error fetching database version');
+  }
+});
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 10000;
+const server = http.createServer(app);
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
