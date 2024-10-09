61cd1799a017c97938dd0e70cee8a007aeff0a53
timataliatov <ttaliatov@gmail.com>
2024-10-09 16:25:50 +0200

    db changes

--- src/config/db.js
+++ src/config/db.js
@@ -2,6 +2,11 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   ssl: {
     rejectUnauthorized: false
   }
@@ -10,3 +15,7 @@ const pool = new Pool({
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
+
+pool.connect()
+  .then(() => console.log('Connected to Neon PostgreSQL'))
+  .catch(err => console.error('Connection error', err.stack));
